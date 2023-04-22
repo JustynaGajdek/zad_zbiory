86fe49ae94efbeee2e2385dda72d5eef30d5d628
JustynaGajdek <131193212+JustynaGajdek@users.noreply.github.com>
2023-04-22 13:03:39 +0200

    zmiana

---
+++ zad_zbiory/src/main/java/org/example/App.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        // Stwórz dwa HashSety integerów i zaimplementuj dla nich następujące działania rachunków zbiorów:
+        // suma, część wspólna, różnica, suma bez części wspólnej.
+        // Wynik działania powinien trafić do trzeciego zbioru.
+
+        Set<Integer> setA= new HashSet<>();
+        setA.add(1);
+        setA.add(5);
+        setA.add(6);
+        setA.add(7);
+
+        Set<Integer> setB= new HashSet<>();
+        setA.add(6);
+        setA.add(3);
+        setA.add(9);
+        setA.add(7);
+        setA.add(4);
+
+
+
+    }
+}
